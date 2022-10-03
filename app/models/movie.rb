5fdf04847450c2094e33f07329b90c49153d9d97
Hary Pirajan Mahalingam <harypirajan@berkeley.edu>
2022-10-03 00:24:11 +0000

    part 1 complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,16 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings 
+    Movie
+        .select(:rating)
+        .distinct
+        .to_a
+        .map { |x| x.rating }
+  end
+
+  def self.with_ratings(ratings_list)
+    Movie
+      .where(rating: ratings_list)
+  end
+
 end
